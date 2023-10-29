fb776c06c9a3ccd8f451a400cd0f2d0a46d1decb
iestyn129 <iestyn129@msmhacks.uk>
2023-10-29 10:00:47 +0000

    commit message

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import customtkinter as ctk
+import pyperclip
 
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("dark-blue")
 
 root = ctk.CTk()
-root.geometry("256x208")
+root.geometry("256x256")
 root.title("FC-GEN")
 root.resizable(False, False)
 
@@ -16,12 +17,16 @@ def get_friend_code(bbb_id: int) -> str:
 
 
 def copyfc():
-    print("Copied")
+    print("Copied " + fc.upper())
+    pyperclip.copy(fc.upper())
+    cbutton.configure(text="Copied!")
+    print("-------------------")
 
 
 def gen():
     # title2.configure(text="no fuck you")
     print("Gen Clicked")
+    cbutton.configure(text="Copy")
     if identry.get().isupper() or identry.get().islower():
         title2.configure(text="ID Cannot Contain Letters")
         print("ID Has Letters")
@@ -35,14 +40,17 @@ def gen():
         bbb_id = int(identry.get())
         b = bbb_id * 11 // 14 % 14
         c = bbb_id * 11 % 14
+        global fc
         fc = str(bbb_id) + chr(ord("a") + b) + chr(ord("a") + c)
         if len(fc) == 10:
             title2.configure(text=(fc.upper() + " (Mobile)"))
             print("ID Is Old Mobile")
+            cbutton.configure(state="normal")
         elif len(fc) == 11:
             if fc.startswith("1"):
                 title2.configure(text=(fc.upper() + " (Mobile)"))
                 print("ID Is New Mobile 11")
+                cbutton.configure(state="normal")
             else:
                 title2.configure(text="ID Invalid")
                 print("ID Is Invalid")
@@ -50,6 +58,7 @@ def gen():
             if fc.startswith("1"):
                 title2.configure(text=(fc.upper() + " (Mobile)"))
                 print("ID Is New Mobile 12")
+                cbutton.configure(state="normal")
             elif fc.startswith("4"):
                 title2.configure(text=(fc.upper() + " (Steam)"))
                 print("ID Is Steam")
@@ -60,6 +69,8 @@ def gen():
             # title2.configure(text=(fc.upper() + " wait fuck what"))
             title2.configure(text="ID Invalid")
             print("ID Is Invalid")
+
+        print("-------------------")
 
 
 frame = ctk.CTkFrame(master=root)
@@ -76,5 +87,8 @@ button.pack(pady=10, padx=5)
 
 title2 = ctk.CTkLabel(master=frame, text="", font=("Nunito", 14))
 title2.pack(pady=5, padx=5)
+
+cbutton = ctk.CTkButton(master=frame, text="Copy", command=copyfc, font=("Nunito", 14), state="disabled")
+cbutton.pack(pady=10, padx=5)
 
 root.mainloop()
---
+++ venv/Lib/site-packages/timer/__init__.py
@@ -0,0 +1,4 @@
+from .__version__ import __version__, __version_info__
+from .timer import get_timer
+
+timer = get_timer()
